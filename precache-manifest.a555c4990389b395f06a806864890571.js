865ed852d3a83eca30feb9643f432822c70e4c03
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-06-06 06:55:38 -0500

    Updates

---
+++ precache-manifest.a555c4990389b395f06a806864890571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be1917b0ddfb9299a6c1861f27b35d9",
+    "url": "/JavaScript-Calculator/index.html"
+  },
+  {
+    "revision": "10480fbb7b226bc10989",
+    "url": "/JavaScript-Calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c4ccee5aa4e431f5cb07",
+    "url": "/JavaScript-Calculator/static/css/main.34b98ed6.chunk.css"
+  },
+  {
+    "revision": "10480fbb7b226bc10989",
+    "url": "/JavaScript-Calculator/static/js/2.d37f50c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JavaScript-Calculator/static/js/2.d37f50c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ccee5aa4e431f5cb07",
+    "url": "/JavaScript-Calculator/static/js/main.ccfd6066.chunk.js"
+  },
+  {
+    "revision": "1b7df05fdc7a7bbf2d7e",
+    "url": "/JavaScript-Calculator/static/js/runtime-main.77f9921f.js"
+  }
+]);
